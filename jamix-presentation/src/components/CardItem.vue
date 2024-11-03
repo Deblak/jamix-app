bec9fc3577644fd9039280f64ac093332d25b55f
Deblak <deblak@mailo.com>
2024-11-03 21:34:34 +0100

    Axios fetch homepage #10

--- jamix-presentation/src/components/CardItem.vue
+++ jamix-presentation/src/components/CardItem.vue
@@ -1,15 +1,25 @@
+<script setup>
+
+defineProps({
+    id: Number,
+    title: String,
+    createdAt: String
+});
+
+</script>
+
 <template>
     <div class="card mb-5 col-lg-3">
-        <img src="../../public/elizeu-dias-29QO6oX3GlA-unsplash.jpg" class="card-img-top" alt="Ad picture">
+        <img src="../../elizeu-dias-29QO6oX3GlA-unsplash.jpg" class="card-img-top" alt="Ad picture">
         <div class="card-body">
-            <h5 class="card-title title-2">Cherche chanteur H/F</h5>
+            <h5 class="card-title title-2">{{ title }}</h5>
             <p class="txt-body color-primary m-0">Poitiers - 86000</p>
             <ul class="card-txt txt-body">
                 <li class="m-1 theme-secondary">Chant</li>
                 <li class="m-1 theme-tertiary">Pop Rock</li>
                 <li class="m-1 theme-soft">Monter un groupe</li>
             </ul>
-            <p class="mt-3 mb-0 txt-body color-soft">27/05/24</p>
+            <p class="mt-3 mb-0 txt-body color-soft">{{ createdAt }}</p>
         </div>
 
         <div class="card-footer txt-body-secondary bg-light">
