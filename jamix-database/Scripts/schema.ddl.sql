a26b8bf8960b66a585055bb5a01ba6c3b6a031a1
Deblak <deblak@mailo.com>
2025-01-14 22:17:38 +0100

    Create preferences tables for offer (Related to: #47, #48, #52)

--- jamix-database/Scripts/schema.ddl.sql
+++ jamix-database/Scripts/schema.ddl.sql
@@ -1,19 +1,42 @@
+DROP TABLE IF EXISTS t_offers;
 DROP TABLE IF EXISTS t_goals;
 DROP TABLE IF EXISTS t_styles;
 DROP TABLE IF EXISTS t_instruments;
-DROP TABLE IF EXISTS t_offers;
-DROP TABLE IF EXISTS t_users;
+DROP TABLE IF EXISTS t_accounts;
 
- CREATE TABLE t_users(
+ CREATE TABLE t_accounts(
 	id int GENERATED ALWAYS AS IDENTITY,
-	username varchar(255),
+	username varchar(20),
 	email varchar(320),
 	password varchar(72),
-	CONSTRAINT t_users_pkey PRIMARY KEY (id),
-	CONSTRAINT t_users_username_ukey UNIQUE (username),
-	CONSTRAINT t_users_email_ukey UNIQUE (email)
+	CONSTRAINT t_accounts_pkey PRIMARY KEY (id),
+	CONSTRAINT t_accounts_email_ukey UNIQUE (email),
+	CONSTRAINT t_accounts_username_ukey UNIQUE (username)
  );
- -- taille max du mail
+
+CREATE TABLE t_instruments (
+    id_instrument INT GENERATED ALWAYS AS IDENTITY,
+    instrument_name VARCHAR(25) NOT NULL,
+    CONSTRAINT t_instruments_pkey PRIMARY KEY(id_instrument),
+    CONSTRAINT t_instruments_name_ukey UNIQUE (instrument_name)
+
+);
+
+CREATE TABLE t_styles (
+    id_style INT GENERATED ALWAYS AS IDENTITY,
+    style_name VARCHAR(25) NOT NULL,
+    CONSTRAINT t_styles_pkey PRIMARY KEY(id_style),
+    CONSTRAINT t_styles_name_ukey UNIQUE (style_name)
+
+);
+
+CREATE TABLE t_goals (
+    id_goal INT GENERATED ALWAYS AS IDENTITY,
+    goal_name VARCHAR(16) NOT NULL,
+    CONSTRAINT t_goals_pkey PRIMARY KEY(id_goal),
+    CONSTRAINT t_goals_name_ukey UNIQUE (goal_name)
+
+);
 
 CREATE TABLE t_offers(
 	id_offer INT GENERATED ALWAYS AS IDENTITY,
@@ -23,25 +46,12 @@ CREATE TABLE t_offers(
     zip_code char(5),
     contact_email VARCHAR(255) NOT NULL,
 	offer_create_date DATE DEFAULT CURRENT_DATE,
-	CONSTRAINT t_offers_pkey PRIMARY KEY(offer_title)
-);
---taille max desc
-
-CREATE TABLE t_instruments (
-    id_instrument INT GENERATED ALWAYS AS IDENTITY,
-    instrument_name VARCHAR(25) NOT NULL,
-    CONSTRAINT t_instruments_pkey PRIMARY KEY(instrument_name)
-);
---taille max instrument name
-
-CREATE TABLE t_styles (
-    id_style INT GENERATED ALWAYS AS IDENTITY,
-    style_name VARCHAR(25) NOT NULL,
-    CONSTRAINT t_styles_pkey PRIMARY KEY(style_name)
-);
-
-CREATE TABLE t_goals (
-    id_goal INT GENERATED ALWAYS AS IDENTITY,
-    goal_name VARCHAR(16) NOT NULL,
-    CONSTRAINT t_goals_pkey PRIMARY KEY(goal_name)
+	instrument_name VARCHAR(25) NOT NULL,
+   	style_name VARCHAR(25) NOT NULL,
+   	goal_name VARCHAR(16) NOT NULL,
+	CONSTRAINT t_offers_pkey PRIMARY KEY(id_offer),
+	CONSTRAINT t_offers_ukey UNIQUE (offer_title, contact_email),
+	CONSTRAINT t_instruments_fkey FOREIGN KEY(instrument_name) REFERENCES t_instruments(instrument_name),
+   	CONSTRAINT t_styles_fkey FOREIGN KEY(style_name) REFERENCES t_styles(style_name),
+   	CONSTRAINT t_goals_fkey FOREIGN KEY(goal_name) REFERENCES t_goals(goal_name)
 );
