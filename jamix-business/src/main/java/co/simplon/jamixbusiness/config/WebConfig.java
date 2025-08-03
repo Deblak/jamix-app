db0ddef4e83ed76bb25175812c38c1037fc9fb7f
Deblak <deblak@mailo.com>
2025-08-03 14:11:55 +0200

    [Related to: #80] Update CORS

--- jamix-business/src/main/java/co/simplon/jamixbusiness/config/WebConfig.java
+++ jamix-business/src/main/java/co/simplon/jamixbusiness/config/WebConfig.java
@@ -3,6 +3,7 @@ package co.simplon.jamixbusiness.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -17,7 +18,23 @@ public class WebConfig {
     private String allowedOrigins;
 
     @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    @Profile("!prod")
+    SecurityFilterChain devFilterChain(HttpSecurity http) throws Exception {
+	return http.csrf(csrf -> csrf.disable()).authorizeHttpRequests(authorize -> authorize
+		.requestMatchers(HttpMethod.POST, "/account/signup", "/account/login").anonymous()
+		.requestMatchers(HttpMethod.GET, "/offers/owned", "/portfolios/owned").authenticated()
+		.requestMatchers(HttpMethod.GET, "/offers/**", "/api/**", "/images/**", "/portfolios/**").permitAll()
+		.requestMatchers(HttpMethod.POST, "/offers", "/images/**", "/portfolios/owned").authenticated()
+		.requestMatchers(HttpMethod.PATCH, "/offers/**", "/images/**", "/portfolios/owned").authenticated()
+		.requestMatchers(HttpMethod.DELETE, "/offers/**", "/images/**", "/portfolios/owned").authenticated()
+		.requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html")
+		.access(new WebExpressionAuthorizationManager("!environment.acceptsProfiles('prod')")))
+		.oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults())).build();
+    }
+
+    @Bean
+    @Profile("prod")
+    SecurityFilterChain prodFilterChain(HttpSecurity http) throws Exception {
 	return http.cors(Customizer.withDefaults()).csrf(csrf -> csrf.disable())
 		.authorizeHttpRequests(authorize -> authorize
 			.requestMatchers(HttpMethod.POST, "/account/signup", "/account/login").anonymous()
